a2b11096ea7b2603377d032be016aa05b5ba39b3
sinzi-test <sinziana.sebe@gmail.com>
2017-11-15 11:30:08 +0100

    CEdge covered

--- src/test/java/environment/TestCEdge.java
+++ src/test/java/environment/TestCEdge.java
@@ -4,22 +4,29 @@ import org.junit.Before;
 import org.junit.Test;
 
 /**
- * test class
+ * test class for CEdge
  */
-public final class TestCEdge {
+public final class TestCEdge
+{
 
-    private CEdge t_edge;
+    private CEdge m_edge;
 
+    /**
+     * initializing edge
+     */
     @Before
     public void initialize()
     {
-        t_edge = new CEdge( "a b" );
+        m_edge = new CEdge( "a b" );
     }
 
+    /**
+     * testing the constructor
+     */
     @Test
-    public void test_constr()
+    public void constr( )
     {
-        if ( ( t_edge.weight() == 1 ) && ( t_edge.visited() == 0 ) && ( t_edge.about().equals("a b")) )
+        if ( ( m_edge.weight() == 1 ) && ( m_edge.visited() == 0 ) && ( m_edge.about().equals( "a b" ) ) )
         {
             System.out.println( true );
         }
@@ -27,6 +34,102 @@ public final class TestCEdge {
         {
             System.out.println( false );
         }
+    }
+
+    /**
+     * testing the return weight function
+     */
+    @Test
+    public void weight( )
+    {
+        if ( m_edge.weight( ) == 1 )
+        {
+            System.out.println( true );
+        }
+        else
+        {
+            System.out.println( false );
+        }
+    }
+
+    /**
+     * testing the return visits function
+     */
+    @Test
+    public void visited( )
+    {
+        if ( m_edge.visited() == 0 )
+        {
+            System.out.println( true );
+        }
+        else
+        {
+            System.out.println( false );
+        }
+    }
+
+    /**
+     * testing the return info function
+     */
+    @Test
+    public void about( )
+    {
+        if ( m_edge.about().contentEquals( "a b" ) )
+        {
+            System.out.println( true );
+        }
+        else
+        {
+            System.out.println( false );
+        }
+    }
+
+    /**
+     * testing the reset weights and visited function
+     */
+    @Test
+    public void reset()
+    {
+        m_edge.reset();
+        if ( ( m_edge.weight() == 1 ) && ( m_edge.visited() == 0 ) )
+        {
+            System.out.println( true );
+        }
+        else
+        {
+            System.out.println( false );
+        }
+    }
+
+    /**
+     * testing the incrementing visited function
+     */
+    @Test
+    public void add()
+    {
+        m_edge.add();
+        if ( m_edge.visited() == 1 )
+        {
+            System.out.println( true );
+        }
+        else
+        {
+            System.out.println( false );
+        }
+    }
+
+    /**
+     * the main function
+     */
+    public void main()
+    {
+        initialize();
+        constr();
+        about();
+        visited();
+        weight();
+        add();
+        reset();
     }
 
 
--- src/test/java/exec/TestCMain.java
+++ src/test/java/exec/TestCMain.java
@@ -1,20 +1,10 @@
 package exec;
 
-import environment.CEdge;
-import environment.CGraph;
-import environment.CNode;
-import org.junit.Before;
-
 /**
  * test class
  */
 public final class TestCMain
 {
-    private CGraph<?, CNode, CEdge> gm_graph;
 
-    @Before
-    public void initialize() {
-    gm_graph =new CGraph();
-    }
 
 }
