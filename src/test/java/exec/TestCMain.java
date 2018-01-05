1e408451278a8930da39b1002223d97ee39aeb71
sinzi-test <sinziana.sebe@gmail.com>
2018-01-05 11:33:44 +0100

    tests

--- src/test/java/exec/TestCMain.java
+++ src/test/java/exec/TestCMain.java
@@ -1,5 +1,6 @@
 package exec;
 
+import environment.CNode;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
@@ -11,6 +12,7 @@ import java.io.FileReader;
 import java.io.IOException;
 
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assume.assumeNotNull;
 
 
 
@@ -20,11 +22,31 @@ import static org.junit.Assert.assertTrue;
 public class TestCMain
 {
     private CMain m_main;
+    private CNode m_n0;
+    private CNode m_n1;
+    private CNode m_n6;
+    private CNode m_n7;
 
+    /**
+     * initializing te thing
+     * @throws IOException file
+     * @throws ParseException perser
+     */
     @Before
-    public void init()
+    public void init() throws IOException, ParseException
     {
         m_main = new CMain();
+        final JSONParser l_parser = new JSONParser();
+        final JSONObject l_obj = (JSONObject) l_parser.parse( new FileReader( "src/test/resources/Examples/TestMain.json" ) );
+        final JSONArray l_array = (JSONArray) l_obj.get( "nodes" );
+        CNode l_node = new CNode( (JSONObject) l_array.get( 0 ) );
+        m_n0 = l_node;
+        l_node = new CNode( (JSONObject) l_array.get( 1 ) );
+        m_n1 = l_node;
+        l_node = new CNode( (JSONObject) l_array.get( 2 ) );
+        m_n6 = l_node;
+        l_node = new CNode( (JSONObject) l_array.get( 3 ) );
+        m_n7 = l_node;
     }
 
     /**
@@ -36,11 +58,11 @@ public class TestCMain
     public void nodes() throws IOException, ParseException
     {
         final JSONParser l_parser = new JSONParser();
-        final JSONObject l_obj = (JSONObject) l_parser.parse( new FileReader( "src/test/resources/Examples/TestGraph.json" ) );
+        final JSONObject l_obj = (JSONObject) l_parser.parse( new FileReader( "src/test/resources/Examples/TestMain.json" ) );
         final JSONArray l_array = (JSONArray) l_obj.get( "nodes" );
         CMain.createNodes( l_array );
-        assertTrue( m_main.s_idcounter == 2 );
-        assertTrue( m_main.s_GR.countNodes() == 2 );
+        assertTrue( m_main.s_idcounter == 4 );
+        assertTrue( m_main.s_GR.countNodes() == 4 );
     }
 
     /**
@@ -75,5 +97,140 @@ public class TestCMain
         l_array = (JSONArray) l_obj.get( "edges" );
 
     }
+
+    /**
+     * testing the get new coordinates horizontal right
+     */
+    @Test
+    public void getCoordHR()
+    {
+        final JSONObject l_funct = new JSONObject();
+        l_funct.put( "type", CMain.calculateOrientation( m_n0, m_n1 ) );
+        l_funct.put( "parameters", CMain.getab( m_n0, m_n1, (String) l_funct.get( "type" ) ) );
+        final Double l_dist = Double.valueOf( 10 );
+        final JSONObject l_res = CMain.getCoordHori( l_funct, m_n0.firstCoord(), m_n0.secondCoord(), l_dist );
+        assertTrue( (double) l_res.get( "x" ) == 10.0 );
+        assertTrue( (double) l_res.get( "y" ) == 0.0 );
+    }
+
+    /**
+     * testing the get new coordinates horizontal left
+     */
+    @Test
+    public void getCoordHL()
+    {
+        assumeNotNull( m_n0 );
+        assumeNotNull( m_n1 );
+        final JSONObject l_funct = new JSONObject();
+        l_funct.put( "type", CMain.calculateOrientation( m_n1, m_n0 ) );
+        l_funct.put( "parameters", CMain.getab( m_n1, m_n0, (String) l_funct.get( "type" ) ) );
+        final Double l_dist = Double.valueOf( 10 );
+        final JSONObject l_res = CMain.getCoordHori( l_funct, m_n1.firstCoord(), m_n1.secondCoord(), l_dist );
+        assertTrue( (double) l_res.get( "x" ) == 90.00 );
+        assertTrue( (double) l_res.get( "y" ) == 0.00 );
+    }
+
+    /**
+     * testing the get new coordinates vertical ascending
+     */
+    @Test
+    public void getCoordVA()
+    {
+        assumeNotNull( m_n0 );
+        assumeNotNull( m_n6 );
+        final JSONObject l_funct = new JSONObject();
+        l_funct.put( "type", CMain.calculateOrientation( m_n0, m_n6 ) );
+        l_funct.put( "parameters", CMain.getab( m_n0, m_n6, (String) l_funct.get( "type" ) ) );
+        final Double l_dist = Double.valueOf( 10 );
+        final JSONObject l_res = CMain.getCoordVert( l_funct, m_n0.firstCoord(), m_n0.secondCoord(), l_dist );
+        assertTrue( (double) l_res.get( "x" ) == 0.00 );
+        assertTrue( (double) l_res.get( "y" ) == 10.00 );
+    }
+
+    /**
+     * testing the get new coordinates vertical descending
+     */
+    @Test
+    public void getCoordVD()
+    {
+        assumeNotNull( m_n0 );
+        assumeNotNull( m_n6 );
+        final JSONObject l_funct = new JSONObject();
+        l_funct.put( "type", CMain.calculateOrientation( m_n6, m_n0 ) );
+        l_funct.put( "parameters", CMain.getab( m_n6, m_n0, (String) l_funct.get( "type" ) ) );
+        final Double l_dist = Double.valueOf( 10 );
+        final JSONObject l_res = CMain.getCoordVert( l_funct, m_n6.firstCoord(), m_n6.secondCoord(), l_dist );
+        assertTrue( (double) l_res.get( "x" ) == 0.00 );
+        assertTrue( (double) l_res.get( "y" ) == 65.0 );
+    }
+
+    /**
+     * testing the get new coordinates ascending right
+     */
+    @Test
+    public void getCoordAR()
+    {
+        assumeNotNull( m_n0 );
+        assumeNotNull( m_n7 );
+        final JSONObject l_funct = new JSONObject();
+        l_funct.put( "type", CMain.calculateOrientation( m_n0, m_n7 ) );
+        l_funct.put( "parameters", CMain.getab( m_n0, m_n7, (String) l_funct.get( "type" ) ) );
+        final Double l_dist = Double.valueOf( 10 );
+        final JSONObject l_res = CMain.getCoordLinear( l_funct, m_n0.firstCoord(), m_n0.secondCoord(), l_dist );
+        assertTrue( (double) l_res.get( "x" ) == 8.00 );
+        assertTrue( (double) l_res.get( "y" ) == 6.00 );
+    }
+
+    /**
+     * testing the get new coordinates descending right
+     */
+    @Test
+    public void getCoordDR()
+    {
+        assumeNotNull( m_n6 );
+        assumeNotNull( m_n1 );
+        final JSONObject l_funct = new JSONObject();
+        l_funct.put( "type", CMain.calculateOrientation( m_n6, m_n1 ) );
+        l_funct.put( "parameters", CMain.getab( m_n6, m_n1, (String) l_funct.get( "type" ) ) );
+        final Double l_dist = Double.valueOf( 10 );
+        final JSONObject l_res = CMain.getCoordLinear( l_funct, m_n6.firstCoord(), m_n6.secondCoord(), l_dist );
+        assertTrue( (double) l_res.get( "x" ) == 8.00 );
+        assertTrue( (double) l_res.get( "y" ) == 69.00 );
+    }
+
+    /**
+     * testing the get new coordinates ascending left
+     */
+    @Test
+    public void getCoordAL()
+    {
+        assumeNotNull( m_n1 );
+        assumeNotNull( m_n6 );
+        final JSONObject l_funct = new JSONObject();
+        l_funct.put( "type", CMain.calculateOrientation( m_n1, m_n6 ) );
+        l_funct.put( "parameters", CMain.getab( m_n1, m_n6, (String) l_funct.get( "type" ) ) );
+        final Double l_dist = Double.valueOf( 10 );
+        final JSONObject l_res = CMain.getCoordLinear( l_funct, m_n1.firstCoord(), m_n1.secondCoord(), l_dist );
+        assertTrue( (double) l_res.get( "x" ) == 92.00 );
+        assertTrue( (double) l_res.get( "y" ) == 6.00 );
+    }
+
+    /**
+     * testing the get new coordinates descending left
+     */
+    @Test
+    public void getCoordDL()
+    {
+        assumeNotNull( m_n0 );
+        assumeNotNull( m_n7 );
+        final JSONObject l_funct = new JSONObject();
+        l_funct.put( "type", CMain.calculateOrientation( m_n7, m_n0 ) );
+        l_funct.put( "parameters", CMain.getab( m_n7, m_n0, (String) l_funct.get( "type" ) ) );
+        final Double l_dist = Double.valueOf( 10 );
+        final JSONObject l_res = CMain.getCoordLinear( l_funct, m_n7.firstCoord(), m_n7.firstCoord(), l_dist );
+        assertTrue( (double) l_res.get( "x" ) == 92.00 );
+        assertTrue( (double) l_res.get( "y" ) == 69.0 );
+    }
+
 
 }
