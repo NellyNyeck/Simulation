8b34f72b96691aadd36974419af0175712bb1990
sinziana-sebe <sinziana.sebe@gmail.com>
2018-01-25 13:17:47 +0100

    testing

--- src/test/java/org/socialcars/sinziana/simulation/data/TestCInput.java
+++ src/test/java/org/socialcars/sinziana/simulation/data/TestCInput.java
@@ -6,10 +6,12 @@ import org.junit.Assert;
 import org.junit.Assume;
 import org.junit.Before;
 import org.junit.Test;
+import org.socialcars.sinziana.simulation.data.input.CAgent;
 import org.socialcars.sinziana.simulation.data.input.CInput;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Map;
 
 
 /**
@@ -56,11 +58,29 @@ public final class TestCInput
     /**
      * test scenario agents
      */
+    @Test
     public void testagents()
     {
         Assume.assumeNotNull( m_configuration );
-
         Assert.assertNotNull( m_configuration.getAgents() );
+        Assert.assertTrue( m_configuration.getAgents().size() == 1 );
+        final CAgent l_prov = m_configuration.getAgents().get( 0 );
+        Assert.assertNotNull( l_prov );
+        Assert.assertTrue( l_prov.getName().contentEquals( "DHL" ) );
+        Assert.assertTrue( l_prov.getFilename().contentEquals( "" ) );
+        final Map<String, Object> l_extra = l_prov.getAdditionalProperties();
+        Assert.assertNotNull( l_extra.get( "agent-type" ) );
+        Assert.assertTrue( l_extra.get( "agent-type" ).equals( "provider" ) );
+        Assert.assertNotNull( l_extra.get( "colour" ) );
+        Assert.assertTrue( l_extra.get( "colour" ).equals( "yellow" ) );
+        Assert.assertNotNull( l_extra.get( "available pods" ) );
+        Assert.assertTrue( l_extra.get( "available pods" ).equals( 1 ) );
+        Assert.assertNotNull( l_extra.get( "maximum number of customers" ) );
+        Assert.assertTrue( l_extra.get( "maximum number of customers" ).equals( 1 ) );
+        Assert.assertNotNull( l_extra.get( "maximum outgoing pods/time unit" ) );
+        Assert.assertTrue( l_extra.get( "maximum outgoing pods/time unit" ).equals( 1 ) );
+        //final List<>l_depots = l_extra.get( "depots" );
+
         // @todo test agent data
     }
 
