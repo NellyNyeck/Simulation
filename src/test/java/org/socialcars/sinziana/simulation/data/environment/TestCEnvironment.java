2135396c60d92da4e848343eca49ed61128fdb1f
Philipp Kraus <philipp.kraus@flashpixx.de>
2018-02-05 17:00:44 +0100

    static projection

--- src/test/java/org/socialcars/sinziana/simulation/data/environment/TestCEnvironment.java
+++ src/test/java/org/socialcars/sinziana/simulation/data/environment/TestCEnvironment.java
@@ -3,6 +3,8 @@ package org.socialcars.sinziana.simulation.data.environment;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import edu.uci.ics.jung.algorithms.layout.FRLayout;
 import edu.uci.ics.jung.algorithms.layout.Layout;
+import edu.uci.ics.jung.algorithms.layout.util.RandomLocationTransformer;
+import edu.uci.ics.jung.graph.Graph;
 import edu.uci.ics.jung.visualization.VisualizationViewer;
 import edu.uci.ics.jung.visualization.renderers.BasicRenderer;
 import org.junit.Before;
@@ -15,8 +17,11 @@ import org.socialcars.sinziana.simulation.environment.INode;
 
 import javax.swing.JFrame;
 import javax.swing.Renderer;
+import java.awt.*;
+import java.awt.geom.Dimension2D;
 import java.io.File;
 import java.io.IOException;
+import java.io.UncheckedIOException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.IntStream;
@@ -27,6 +32,20 @@ import java.util.stream.IntStream;
  */
 public final class TestCEnvironment
 {
+    private static CInputpojo m_input;
+
+    static
+    {
+        try
+        {
+            m_input = new ObjectMapper().readValue( new File( "src/test/resources/example_input.json" ), CInputpojo.class );
+        }
+        catch ( final IOException l_exception )
+        {
+            throw new UncheckedIOException( l_exception );
+        }
+    }
+
     private CEnvironment m_env;
 
     /**
@@ -36,7 +55,7 @@ public final class TestCEnvironment
     @Before
     public void init() throws IOException
     {
-        m_env = new CEnvironment( new ObjectMapper().readValue( new File( "src/test/resources/example_input.json" ), CInputpojo.class ).getGraph() );
+        m_env = new CEnvironment( m_input.getGraph() );
     }
 
     @Test
@@ -58,14 +77,20 @@ public final class TestCEnvironment
         System.out.println( l_heatmap );
     }
 
-    @Test
     public static void main( final String[] p_args ) throws IOException
     {
-        final IEnvironment l_env = new CEnvironment( new ObjectMapper().readValue( new File( "src/test/resources/example_input.json" ), CInputpojo.class ).getGraph() );
-
         final JFrame l_frame = new JFrame();
-        l_frame.getContentPane().add ( new VisualizationViewer<>( new FRLayout<>( l_env.getGraph() ) ) ); ;
+        l_frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+        l_frame.setSize( new Dimension( 1200, 1200 ) );
+
+
+        final IEnvironment l_env = new CEnvironment( m_input.getGraph() );
+
+        final FRLayout<INode, IEdge> l_projection = new FRLayout<>( l_env.getGraph(), l_frame.getSize() );
+        l_projection.setInitializer( new RandomLocationTransformer<>( l_frame.getSize(), 1 ) );
+
+        l_frame.getContentPane().add ( new VisualizationViewer<>( l_projection ) );
         l_frame.setVisible( true );
-        l_frame.pack();
     }
+
 }
