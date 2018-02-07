25d1f965d9f790ce548b619595460248c25567c8
Philipp Kraus <philipp.kraus@flashpixx.de>
2018-02-07 21:21:11 +0100

    graphhopper download

--- src/main/java/org/socialcars/sinziana/simulation/environment/osm/COSMEnvironment.java
+++ src/main/java/org/socialcars/sinziana/simulation/environment/osm/COSMEnvironment.java
@@ -14,10 +14,12 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.UncheckedIOException;
 import java.io.UnsupportedEncodingException;
 import java.net.URL;
 import java.nio.channels.Channels;
 import java.nio.channels.ReadableByteChannel;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.security.MessageDigest;
@@ -36,19 +38,13 @@ public class COSMEnvironment implements IEnvironment<JXMapViewer>
     /**
      * graphhopper instance
      */
-    private final GraphHopper m_graph = new GraphHopperOSM().forServer();
+    private final CGraphHopper m_graph;
 
 
 
     public COSMEnvironment( final String p_downloadurl )
     {
-        final File l_storagepath = storage( p_downloadurl ).toFile();
-        if ( !l_storagepath.exists() )
-
-        m_graph.setGraphHopperLocation( l_storagepath.toString() );
-        m_graph.setEncodingManager( new EncodingManager( "car") );
-        m_graph.importOrLoad();
-
+        m_graph = new CGraphHopper( p_downloadurl );
     }
 
     @Override
@@ -101,62 +97,113 @@ public class COSMEnvironment implements IEnvironment<JXMapViewer>
     }
 
     /**
-     * downloads the OSM data
-     *
-     * @param p_url URL for downloading as string
-     * @return download file with full path
+     * modified graphhopper structure
      */
-    private static File downloadosmpbf( final String p_url ) throws IOException
+    private static final class CGraphHopper extends GraphHopper
     {
-        final File l_output = File.createTempFile( "openstreetmap", ".osm.pbf" );
-        final URL l_url = new URL( p_url );
 
-        final ReadableByteChannel l_channel = Channels.newChannel( l_url.openStream() );
-        final FileOutputStream l_stream = new FileOutputStream( l_output );
-        l_stream.getChannel().transferFrom( l_channel, 0, Long.MAX_VALUE );
-
-        return l_output;
-    }
-
-    /**
-     * creates a storage of the graph
-     *
-     * @param p_downloadurl url
-     * @return path
-     */
-    private static Path storage( final String p_downloadurl )
-    {
-        try
+        CGraphHopper( final String p_downloadurl )
         {
-            return Paths.get(
-                System.getProperty("user.home"),
-                ".osmstorage",
+            final File l_graphlocation = storage( p_downloadurl ).toFile();
+            try
+            {
+                if ( !this.load( l_graphlocation.getAbsolutePath() ) )
+                    this.downloadandload( p_downloadurl, l_graphlocation );
 
-                getBytes2Hex(
-                    MessageDigest.getInstance( "MD5" )
-                                 .digest( p_downloadurl.getBytes( "UTF-8" ) )
-                )
-
-            );
+            }
+            catch ( final Exception l_exception )
+            {
+                this.downloadandload( p_downloadurl, l_graphlocation );
+            }
         }
-        catch ( final UnsupportedEncodingException | NoSuchAlgorithmException l_exception )
+
+        /**
+         * download graph and run converting
+         *
+         * @param p_url download URL
+         * @param p_directory directory in which the graph data is stored
+         */
+        private void downloadandload( final String p_url, final File p_directory )
         {
-            throw new RuntimeException( l_exception );
+            try
+            {
+                this.setDataReaderFile( downloadosmpbf(p_url).getAbsolutePath() );
+                this.setGraphHopperLocation( p_directory.toString() );
+                this.setEncodingManager( new EncodingManager( "car") );
+
+                this.initDataReader( this.importData() );
+            }
+            catch ( final IOException l_exception )
+            {
+                throw new RuntimeException( l_exception );
+            }
         }
-    }
 
-    /**
-     * returns a string with hexadecimal bytes
-     *
-     * @param p_bytes input bytes
-     * @return hexadecimal string
-     */
-    private static String getBytes2Hex( final byte[] p_bytes )
-    {
-        final StringBuilder l_str = new StringBuilder( 2 * p_bytes.length );
-        for ( final byte l_byte : p_bytes )
-            l_str.append( String.format( "%02x", l_byte & 0xff ) );
+        /**
+         * downloads the OSM data
+         *
+         * @param p_url URL for downloading as string
+         * @return download file with full path
+         */
+        private static File downloadosmpbf( final String p_url ) throws IOException
+        {
+            final File l_output = File.createTempFile( "openstreetmap", ".osm.pbf" );
+            final URL l_url = new URL( p_url );
 
-        return l_str.toString();
+            final ReadableByteChannel l_channel = Channels.newChannel( l_url.openStream() );
+            final FileOutputStream l_stream = new FileOutputStream( l_output );
+            l_stream.getChannel().transferFrom( l_channel, 0, Long.MAX_VALUE );
+
+            return l_output;
+        }
+
+        /**
+         * creates a storage of the graph
+         *
+         * @param p_downloadurl url
+         * @return path
+         */
+        private static Path storage( final String p_downloadurl )
+        {
+            try
+            {
+                final Path l_path = Paths.get(
+                        System.getProperty("user.home"),
+                        ".osmstorage",
+
+                        getBytes2Hex(
+                                MessageDigest.getInstance( "MD5" )
+                                        .digest( p_downloadurl.getBytes( "UTF-8" ) )
+                        )
+
+                );
+
+                Files.createDirectories(l_path );
+                return l_path;
+            }
+            catch ( final UnsupportedEncodingException | NoSuchAlgorithmException l_exception )
+            {
+                throw new RuntimeException( l_exception );
+            }
+            catch ( final IOException l_exception )
+            {
+                throw new UncheckedIOException( l_exception );
+            }
+        }
+
+        /**
+         * returns a string with hexadecimal bytes
+         *
+         * @param p_bytes input bytes
+         * @return hexadecimal string
+         */
+        private static String getBytes2Hex( final byte[] p_bytes )
+        {
+            final StringBuilder l_str = new StringBuilder( 2 * p_bytes.length );
+            for ( final byte l_byte : p_bytes )
+                l_str.append( String.format( "%02x", l_byte & 0xff ) );
+
+            return l_str.toString();
+        }
     }
 }
