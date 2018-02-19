6294e4bc0d10cfe10c6d8ce8a115dee968c77a01
sinziana-sebe <sinziana.sebe@gmail.com>
2018-02-19 16:57:58 +0100

    barcelona

--- src/main/java/org/socialcars/sinziana/simulation/CMain.java
+++ src/main/java/org/socialcars/sinziana/simulation/CMain.java
@@ -28,9 +28,5 @@ public final class CMain
      */
     public static void main( final String[] p_args ) throws IOException
     {
-        GraphHopperStorage m_storage = new GraphHopperStorage( new RAMDirectory(), new EncodingManager("car"), false, new GraphExtension.NoOpExtension() );
-        OSMReader m_reader = new OSMReader(m_storage);
-        m_reader.setFile( new File( "src/test/resources/new-york-latest.osm.pbf" ));
-        m_reader.readGraph();
     }
 }
