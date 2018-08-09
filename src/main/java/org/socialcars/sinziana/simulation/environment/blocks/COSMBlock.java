e87aa66c8d28a3216ac741a2576fe98ecdf82d46
sinziana-sebe <sinziana.sebe@gmail.com>
2018-08-09 12:26:39 +0200

    osm block update

--- src/main/java/org/socialcars/sinziana/simulation/environment/blocks/COSMBlock.java
+++ src/main/java/org/socialcars/sinziana/simulation/environment/blocks/COSMBlock.java
@@ -1,6 +1,9 @@
 package org.socialcars.sinziana.simulation.environment.blocks;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import edu.uci.ics.jung.algorithms.shortestpath.DijkstraShortestPath;
+import edu.uci.ics.jung.graph.DirectedGraph;
+import edu.uci.ics.jung.graph.DirectedSparseMultigraph;
 import org.jxmapviewer.viewer.GeoPosition;
 import org.socialcars.sinziana.simulation.data.input.CStreetpojo;
 import org.socialcars.sinziana.simulation.data.input.CStreetsetpojo;
@@ -10,7 +13,10 @@ import org.socialcars.sinziana.simulation.environment.osm.CStreetStructure;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 
@@ -20,13 +26,15 @@ import java.util.stream.IntStream;
 public class COSMBlock implements IBlockEnv
 {
     private static final Double RADIUS = 6371e3;
+    private static final String CONNECTOR = "-";
 
     private final Double m_blocksize;
-    private ArrayList<CBlock> m_blocks;
+    private HashMap<String, CBlock> m_blocks;
     private final String m_connector;
     private final String m_filename;
 
-
+    private final DirectedGraph<IBlock, String> m_graph;
+    private final DijkstraShortestPath<IBlock, String> m_pathalgorithm;
 
     /**
      * ctor
@@ -35,9 +43,11 @@ public class COSMBlock implements IBlockEnv
     public COSMBlock( final Double p_blocksize, final String p_file )
     {
         m_blocksize = p_blocksize;
-        m_blocks = new ArrayList<>();
+        m_blocks = new HashMap<>();
         m_connector = "_";
         m_filename = p_file;
+        m_graph = new DirectedSparseMultigraph<>();
+        m_pathalgorithm = new DijkstraShortestPath<>( m_graph, null );
         map();
     }
 
@@ -59,16 +69,21 @@ public class COSMBlock implements IBlockEnv
                 {
                     final CBlock l_new = new CBlock( s.start().getLatitude() + m_connector + s.start().getLongitude(),
                         s.start().getLatitude(), s.start().getLongitude()  );
-                    m_blocks.add( l_new );
+                    m_blocks.put( l_new.id(), l_new );
+                    m_graph.addVertex( l_new );
                 }
                 else if ( l_dist / m_blocksize < 2 )
                 {
                     final CBlock l_new1 = new CBlock( s.start().getLatitude() + m_connector + s.start().getLongitude(),
                         s.start().getLatitude(), s.start().getLongitude()  );
-                    m_blocks.add( l_new1 );
+                    m_blocks.put( l_new1.id(), l_new1 );
+                    m_graph.addVertex( l_new1 );
                     final CBlock l_new2 = new CBlock( s.end().getLatitude() + m_connector + s.end().getLongitude(),
                         s.end().getLatitude(), s.end().getLongitude()  );
-                    m_blocks.add( l_new2 );
+                    m_blocks.put( l_new2.id(), l_new2 );
+                    m_graph.addVertex( l_new2 );
+                    m_graph.addEdge( l_new1.id() + CONNECTOR + l_new2.id(), l_new1, l_new2 );
+                    m_graph.addEdge( l_new2.id() + CONNECTOR + l_new1.id(), l_new2, l_new1 );
                     connectwithBearing( l_bearingb, l_new1, l_new2 );
 
                 }
@@ -90,10 +105,12 @@ public class COSMBlock implements IBlockEnv
     {
         final CBlock l_start = new CBlock( p_struct.start().getLatitude() + m_connector + p_struct.start().getLongitude(),
             p_struct.start().getLatitude(), p_struct.start().getLongitude()  );
-        m_blocks.add( l_start );
+        m_blocks.put( l_start.id(), l_start );
+        m_graph.addVertex( l_start );
         final CBlock l_end = new CBlock( p_struct.end().getLatitude() + m_connector + p_struct.end().getLongitude(),
             p_struct.end().getLatitude(), p_struct.end().getLongitude()  );
-        m_blocks.add( l_end );
+        m_blocks.put( l_end.id(), l_end );
+        m_graph.addVertex( l_end );
         final CBlock[] l_temp = {l_start};
         IntStream.range( 1, (int) ( p_dist / m_blocksize  + 1 ) )
             .boxed()
@@ -101,10 +118,13 @@ public class COSMBlock implements IBlockEnv
             {
                 final GeoPosition l_pos = calculateNext( new GeoPosition( l_temp[0].get1(), l_temp[0].get2() ), m_blocksize, p_bearing );
                 final CBlock l_new = new CBlock( l_pos.getLatitude() + "_" + l_pos.getLongitude(), l_pos.getLatitude(), l_pos.getLongitude() );
-                m_blocks.add( l_new );
+                m_blocks.put( l_new.id(), l_new );
+                m_graph.addVertex( l_new );
+                m_graph.addEdge( l_temp[0].id() + CONNECTOR + l_new.id(), l_temp[0], l_new );
                 connectwithBearing( p_bearing, l_temp[0], l_new );
                 l_temp[0] = l_new;
             } );
+        m_graph.addEdge( l_temp[0].id(), l_temp[0], l_end );
         connectwithBearing( p_bearing, l_temp[0], l_end );
     }
 
@@ -134,9 +154,9 @@ public class COSMBlock implements IBlockEnv
 
     private void connecttheBlocks()
     {
-        m_blocks.forEach( b ->
+        m_blocks.values().forEach( b ->
         {
-            m_blocks.forEach( bl ->
+            m_blocks.values().forEach( bl ->
             {
                 if ( ( ( calculateDistance( new GeoPosition( b.get1(), b.get2() ), new GeoPosition( bl.get1(), bl.get2() ) ) ) < 0.000009004 )
                     && ( !b.isNeighbour( bl ) ) )
@@ -216,6 +236,18 @@ public class COSMBlock implements IBlockEnv
     @Override
     public void move( final IMovable p_agent )
     {
+        final CBlock l_pos = m_blocks.get( p_agent.position() );
+        final CBlock l_end = m_blocks.get( p_agent.destination() );
+        final List<String> l_route = m_pathalgorithm.getPath( l_pos, l_end ).stream().collect( Collectors.toList() );
+        Double l_speed = p_agent.speed();
+        final Double l_accel = p_agent.acceleration();
+        while ( m_blocks.get( l_route.get( l_speed.intValue() ) ).occupied() )
+        {
+            l_speed -= 1;
+            p_agent.accelshift( l_accel - 0.1 );
+        }
+        p_agent.move( l_route.get( l_speed.intValue() ) );
+        m_blocks.get( l_route.get( l_speed.intValue() ) ).occupy( p_agent );
     }
 
     /**
