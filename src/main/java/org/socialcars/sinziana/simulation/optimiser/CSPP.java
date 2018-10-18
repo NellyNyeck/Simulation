653a23f71806c79912731214f01476cc06add810
sinziana-sebe <sinziana.sebe@gmail.com>
2018-10-18 14:19:37 +0200

    single optimiser w weights

---
+++ src/main/java/org/socialcars/sinziana/simulation/optimiser/CSPP.java
@@ -0,0 +1,103 @@
+package org.socialcars.sinziana.simulation.optimiser;
+
+import gurobi.GRB;
+import gurobi.GRBEnv;
+import gurobi.GRBException;
+import gurobi.GRBLinExpr;
+import gurobi.GRBModel;
+import gurobi.GRBVar;
+import org.socialcars.sinziana.simulation.environment.jung.CJungEnvironment;
+import org.socialcars.sinziana.simulation.environment.jung.INode;
+
+import java.util.ArrayList;
+
+public class CSPP
+{
+    private GRBEnv m_env;
+    private GRBModel m_model;
+    private ArrayList<GRBVar> m_variables;
+    private GRBVar[][] m_xs;
+
+
+    public CSPP( final int p_origin, final int p_destination, final CJungEnvironment p_network ) throws GRBException
+    {
+        m_env = new GRBEnv( "spp.log" );
+        m_model = new GRBModel( m_env );
+        m_variables = new ArrayList<>();
+        m_xs = new GRBVar[p_network.size() + 1][p_network.size() + 1];
+
+        final GRBLinExpr l_obj = new GRBLinExpr();
+        p_network.edges().forEach( iEdge ->
+        {
+            final INode l_start = iEdge.from();
+            final INode l_end = iEdge.to();
+            try
+            {
+                m_xs[Integer.valueOf( l_start.id() )][Integer.valueOf( l_end.id() )] = m_model.addVar( 0.0, 1.0, 0.0,
+                    GRB.BINARY,
+                    "x" + l_start.id()  + "_" + l_end.id() );
+                l_obj.addTerm((Double) iEdge.weight(), m_xs[Integer.valueOf( l_start.id() )][Integer.valueOf( l_end.id() )] );
+                m_variables.add( m_xs[Integer.valueOf( l_start.id() )][Integer.valueOf( l_end.id() )] );
+            }
+            catch (GRBException e)
+            {
+                e.printStackTrace();
+            }
+        } );
+        m_model.setObjective( l_obj, GRB.MINIMIZE );
+
+        addConstraints( p_network, p_origin, p_destination );
+        m_model.optimize();
+        display( p_network );
+        System.out.println( "Obj: " + m_model.get( GRB.DoubleAttr.ObjVal ) + " " + l_obj.getValue() );
+        System.out.println();
+
+        m_model.dispose();
+        m_env.dispose();
+    }
+
+    private void addConstraints( final CJungEnvironment p_network, final Integer p_origin, final Integer p_destination ) throws GRBException
+    {
+        m_variables.forEach( c ->
+        {
+            final GRBLinExpr l_expr = new GRBLinExpr();
+            l_expr.addTerm( 1.0, c );
+            try
+            {
+                m_model.addConstr( l_expr, GRB.LESS_EQUAL, 1,
+                    "NetworkConstraint" );
+            }
+            catch ( final GRBException l_err )
+            {
+                l_err.printStackTrace();
+            }
+        } );
+        for ( int i = 0; i < p_network.size(); i++ )
+        {
+            final GRBLinExpr l_expr = new GRBLinExpr();
+            for ( int j = 0; j < p_network.size(); j++ )
+            {
+                if ( m_xs[i][j] != null ) l_expr.addTerm( 1.0, m_xs[i][j] );
+                if ( m_xs[j][i] != null ) l_expr.addTerm( -1.0, m_xs[j][i] );
+            }
+            if ( i == Integer.valueOf( p_origin ) ) m_model.addConstr( l_expr, GRB.EQUAL, 1.0, "OriginConstraint" );
+            else if ( i == Integer.valueOf( p_destination ) ) m_model.addConstr( l_expr, GRB.EQUAL, -1.0, "DestinationConstraint" );
+            else m_model.addConstr( l_expr, GRB.EQUAL, 0.0, "FlowConstraint" );
+        }
+    }
+
+    public void display( final CJungEnvironment p_network ) throws GRBException
+    {
+        for ( int i = 0; i < p_network.size(); i++ )
+        {
+            for ( int j = 0; j < p_network.size(); j++ )
+            {
+                if ( m_xs[i][j] != null && m_xs[i][j].get( GRB.DoubleAttr.X ) == 1 )
+                {
+                    System.out.println( m_xs[i][j].get( GRB.StringAttr.VarName ) + " " + m_xs[i][j].get( GRB.DoubleAttr.X ) );
+                }
+            }
+        }
+    }
+
+}
---
+++ src/test/java/org/socialcars/sinziana/simulation/data/optimiser/TestCSPP.java
@@ -0,0 +1,61 @@
+package org.socialcars.sinziana.simulation.data.optimiser;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import gurobi.GRBException;
+import org.junit.Before;
+import org.junit.Test;
+import org.socialcars.sinziana.simulation.data.input.CInputpojo;
+import org.socialcars.sinziana.simulation.environment.jung.CJungEnvironment;
+import org.socialcars.sinziana.simulation.optimiser.CSPP;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.UncheckedIOException;
+
+public class TestCSPP
+{
+    private static final CInputpojo INPUT;
+
+    private CJungEnvironment m_env;
+    private CSPP m_opt;
+
+    static
+    {
+        try
+        {
+            INPUT = new ObjectMapper().readValue( new File( "src/test/resources/tiergarten_weights.json" ), CInputpojo.class );
+        }
+        catch ( final IOException l_exception )
+        {
+            throw new UncheckedIOException( l_exception );
+        }
+    }
+
+    /**
+     * initializing
+     * @throws IOException file
+     */
+    @Before
+    public void init() throws IOException, GRBException
+    {
+        m_env = new CJungEnvironment( INPUT.getGraph() );
+        m_opt = new CSPP( 1, 24, m_env );
+    }
+
+    @Test
+    public void display() throws GRBException
+    {
+        m_opt.display( m_env );
+    }
+
+    /**
+     * main function
+     * @param p_args cli
+     * @throws IOException file
+     */
+    public static void main( final String[] p_args ) throws IOException, GRBException
+    {
+        final TestCSPP l_test = new TestCSPP();
+        l_test.init();
+    }
+}
